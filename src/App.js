711f5c563cb405ce8572773df9fa885a6264f4df
AhriIy <siy8311@gmail.com>
2020-04-13 01:16:31 +0900

    Create Employee.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Employee from './components/Employee';
+
+const employees = [
+  {
+  'number': '20200413001',
+  'image':'https://placeimg.com/128/128/1',
+  'name': '가나다',
+  'sex': '男性',
+  'position':'社員'
+  },
+  {
+    'number': '20200413002',
+    'image':'https://placeimg.com/128/128/2',
+    'name': '라마바',
+    'sex': '男性',
+    'position':'社員'
+  },
+  {
+    'number': '20200413003',
+    'image':'https://placeimg.com/128/128/3',
+    'name': '사아자',
+    'sex': '女性',
+    'position':'社員'
+  }
+]
 
 function App() {
   return (
-    <div className="background">
-      <img src={logo} lat="logo" />
-      <h2>management system!</h2>
+    <div>
+    {
+      employees.map(e => {
+        return (
+          <Employee
+            key={e.number}
+            number={e.number}
+            image={e.image}
+            name={e.name}
+            sex={e.sex}
+            position={e.position}
+            />)
+      })
+    }
     </div>
   );
 }
