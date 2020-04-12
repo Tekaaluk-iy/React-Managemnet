b517d6426715df1ce7cc7a9cd179bc488d30959d
AhriIy <siy8311@gmail.com>
2020-04-13 01:55:31 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import './App.css';
 import Employee from './components/Employee';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const employees = [
   {
@@ -26,24 +44,37 @@ const employees = [
   }
 ]
 
-function App() {
-  return (
-    <div>
-    {
-      employees.map(e => {
-        return (
-          <Employee
-            key={e.number}
-            number={e.number}
-            image={e.image}
-            name={e.name}
-            sex={e.sex}
-            position={e.position}
-            />)
-      })
-    }
-    </div>
-  );
+class App extends React.Component {
+  render () {
+  const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>写真</TableCell>
+              <TableCell>社員番号</TableCell>
+              <TableCell>名前</TableCell>
+              <TableCell>性別</TableCell>
+              <TableCell>職級</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          { employees.map(e => { return (
+              <Employee
+                key={e.number}
+                image={e.image}
+                number={e.number}
+                name={e.name}
+                sex={e.sex}
+                position={e.position}
+                />)
+          }) }
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,35 +1,19 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Employee extends React.Component {
     render() {
         return (
-            <div>
-                <EmployeeProfile number={this.props.number} image={this.props.image} name={this.props.name}/>
-                <EmployeeInfo sex={this.props.sex} position={this.props.position}/>
-            </div>
+            <TableRow>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.number}</TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.sex}</TableCell>
+                <TableCell>{this.props.position}</TableCell>
+            </TableRow>
         )
     }
 }
 
-class EmployeeProfile extends React.Component {
-    render() {
-        return (
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.number})</h2>
-            </div>
-        )
-    }
-}
-
-class EmployeeInfo extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>{this.props.sex}</p>
-                <p>{this.props.position}</p>
-            </div>
-        )
-    }
-}
 export default Employee;
